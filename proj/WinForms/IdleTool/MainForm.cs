4fe58f8cd610790eb65eaaf3027fbdb32693a838
coder-lloyd <coder.lloyd01@gmail.com>
2025-01-09 16:55:04 +0900

    [proj-WinForms] ## : MainForm * App Controller 객체 관리 Program에서 MainForm으로 이관  * 생성은 초기화 실패를 대비해 앱 종료를 위해 유지

--- proj/WinForms/IdleTool/MainForm.cs
+++ proj/WinForms/IdleTool/MainForm.cs
@@ -2,20 +2,39 @@ namespace IdleTool
 {
     public partial class MainForm : Form
     {
+        Controller.App _appController = null;
+
         int _count = 0;
 
-        public MainForm()
+        public MainForm(Controller.App __app)
         {
             InitializeComponent();
 
             KeyPreview = true;//키 입력 안 먹을 때
             //https://free-sounds.tistory.com/39
+
+            _appController = __app;
+            { Test_App(); }//DEV TEST
+        }
+
+        void Test_App()
+        {
+            //DEV.Tester0.Capture(_appController);
+
+            //DEV.Tester0.Open_FileDialog();
+
+            //PictureBox.Image = Bitmap.FromFile(image_file);
+            //DEV.Tester0.Process_Local_Image();
+
+            //DEV.Tester0.Detect_IconImage_byLocal(_appController);
         }
 
         private void OnClick_Test1(object sender, EventArgs e)
         {
             _count += 1;
             TXT_Logger.Text = $"클릭 {_count}\r\n테스트";
+
+            DEV.Tester0.Detect_IconImage_byLocal(_appController);
         }
 
         void Processs_KeyDown(object sender, KeyEventArgs e)
--- proj/WinForms/IdleTool/Program.cs
+++ proj/WinForms/IdleTool/Program.cs
@@ -1,32 +1,15 @@
 namespace IdleTool
 {
-    using IdleTool.DEV;
     using System;
-    //using System.Diagnostics;
-    using System.Drawing;
-    using System.Drawing.Imaging;
-    //using System.Reflection.Metadata;
-
     using System.Runtime.InteropServices;//DllImport
 
     internal static class Program
     {
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
 #if DEBUG//https://maloveforme.tistory.com/286
         [DllImport("kernel32.dll", SetLastError = true)] static extern bool AllocConsole();
 #endif
 
-        static Controller.App _appController = null;
+        //static Controller.App _appController = null;
 
         /// <summary>
         ///  The main entry point for the application.
@@ -43,38 +26,29 @@ namespace IdleTool
             AllocConsole();
 #endif
 
-            //if (!Initialize())
-            //    return;
+            var appController = Initialize_App();
+            if (null == appController)
+                return;
 
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(appController));
         }
 
-        static bool Initialize()
+        static Controller.App Initialize_App()
         {
             //if (IntPtr.Zero == Detect_GameApp()) return;
 
-            _appController = new Controller.App();
-            if (!_appController.Detect()) return false;
+            var ret = new Controller.App();
+            if (!ret.Detect()) return null;
 
-            var app_rect = _appController.Capture_Rect();
+            var app_rect = ret.Capture_Rect();
             if (!app_rect.IsValid)
             {
                 Console.WriteLine("창 정보를 가져오는 데 실패했습니다.");
-                return false;
+                return null;
             }
-#if DEBUG
-            //DEV.Tester0.Capture(app_controller);
 
-            //DEV.Tester0.Open_FileDialog();
-
-            ////PictureBox.Image = Bitmap.FromFile(image_file);
-            //DEV.Tester0.Process_Local_Image();
-
-            DEV.Tester0.Detect_IconImage_byLocal(_appController);
-#endif
-
-            return true;
+            return ret;
         }
     }
 }
