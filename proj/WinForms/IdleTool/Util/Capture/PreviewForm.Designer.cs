39bef99f627d215ea5b45dbc8081b78cd9d7983b
coder-lloyd <coder.lloyd01@gmail.com>
2025-02-28 14:58:06 +0900

    [proj-WinForms] ## : 캡쳐 - 프리뷰 * Util.Cpature.PreviewForm  * Designer

---
+++ proj/WinForms/IdleTool/Util/Capture/PreviewForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace IdleTool.Util.Capture
+{
+    using System.Windows.Forms;
+
+    partial class PreviewForm
+    {
+        System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        void InitializeComponent()
+        {
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(PreviewForm));
+
+            // PictureBox 설정
+            _picbox = new PictureBox {
+                //Image = capturedImage,
+                SizeMode = PictureBoxSizeMode.AutoSize,
+                Dock = DockStyle.Fill
+            };
+
+            // 저장 버튼 설정
+            _btn_Save = new Button {
+                Text = "저장",
+                Dock = DockStyle.Bottom,
+                Height = 40
+            };
+            _btn_Save.Click += OnClick_Save;
+
+            // 취소 버튼 설정
+            _btn_Cancel = new Button {
+                Text = "취소",
+                Dock = DockStyle.Bottom,
+                Height = 40
+            };
+            _btn_Cancel.Click += OnClick_Cancel;
+
+            {
+                Controls.Add(_picbox);
+
+                Controls.Add(_btn_Save);
+                Controls.Add(_btn_Cancel);
+            }
+
+            {// 폼 기본 설정
+                this.Text = "미리보기";
+                this.StartPosition = FormStartPosition.CenterScreen;
+                this.FormBorderStyle = FormBorderStyle.FixedDialog;
+                this.MaximizeBox = false;
+                this.MinimizeBox = false;
+                this.AutoSize = true;
+                this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            }
+        }
+
+        #region [Controls]
+        PictureBox _picbox;
+
+        Button _btn_Save;
+        Button _btn_Cancel;
+        #endregion
+    }
+}
--- proj/WinForms/IdleTool/Util/Capture/PreviewForm.cs
+++ proj/WinForms/IdleTool/Util/Capture/PreviewForm.cs
@@ -7,63 +7,22 @@
 
     public partial class PreviewForm : Form
     {
-        PictureBox _picbox;
-
-        Button _btn_Save;
-        Button _btn_Cancel;
-
         public PreviewForm(Bitmap capturedImage)
         {
-            // 폼 기본 설정
-            this.Text = "미리보기";
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.AutoSize = true;
-            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-
-            Initialize_Controls();
+            InitializeComponent();
             _picbox.Image = capturedImage;
         }
 
-        void Initialize_Controls()
+        #region [Event Handler]
+        void OnClick_Save(object sender, EventArgs e)
         {
-            // PictureBox 설정
-            _picbox = new PictureBox {
-                //Image = capturedImage,
-                SizeMode = PictureBoxSizeMode.AutoSize,
-                Dock = DockStyle.Fill
-            };
+            _picbox.Image.Save("captured.png", ImageFormat.Png);
 
-            // 저장 버튼 설정
-            _btn_Save = new Button
-            {
-                Text = "저장",
-                Dock = DockStyle.Bottom,
-                Height = 40
-            };
-            _btn_Save.Click += (s, e) => {
-                _picbox.Image.Save("captured.png", ImageFormat.Png);
-
-                this.Close();
-                MessageBox.Show("저장 완료!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            };
-
-            // 취소 버튼 설정
-            _btn_Cancel = new Button {
-                Text = "취소",
-                Dock = DockStyle.Bottom,
-                Height = 40
-            };
-            _btn_Cancel.Click += (s, e) => this.Close();
-
-            {//컨트롤 추가
-                this.Controls.Add(_picbox);
-
-                this.Controls.Add(_btn_Save);
-                this.Controls.Add(_btn_Cancel);
-            }
+            this.Close();
+            MessageBox.Show("저장 완료!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        void OnClick_Cancel(object sender, EventArgs e) => this.Close();
+        #endregion
     }
 }
