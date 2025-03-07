aa130fe0092a461438d4114d3ac947a952f3cef5
coder-lloyd <coder.lloyd01@gmail.com>
2025-03-07 15:16:39 +0900

    [proj-WinForms - ProcessChecker] ## : PID 체커 *  *

---
+++ proj/WinForms/ProcessChecker/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ProcessChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBOX_PID = new System.Windows.Forms.TextBox();
+            this.TBOX_PName = new System.Windows.Forms.TextBox();
+            this.BTN_OK = new System.Windows.Forms.Button();
+            this.BTN_COPY = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TBOX_PID
+            // 
+            this.TBOX_PID.Location = new System.Drawing.Point(12, 12);
+            this.TBOX_PID.Name = "TBOX_PID";
+            this.TBOX_PID.Size = new System.Drawing.Size(143, 21);
+            this.TBOX_PID.TabIndex = 0;
+            // 
+            // TBOX_PName
+            // 
+            this.TBOX_PName.Enabled = false;
+            this.TBOX_PName.Location = new System.Drawing.Point(12, 78);
+            this.TBOX_PName.Name = "TBOX_PName";
+            this.TBOX_PName.Size = new System.Drawing.Size(143, 21);
+            this.TBOX_PName.TabIndex = 1;
+            // 
+            // BTN_OK
+            // 
+            this.BTN_OK.Location = new System.Drawing.Point(177, 12);
+            this.BTN_OK.Name = "BTN_OK";
+            this.BTN_OK.Size = new System.Drawing.Size(75, 23);
+            this.BTN_OK.TabIndex = 2;
+            this.BTN_OK.Text = "확인";
+            this.BTN_OK.UseVisualStyleBackColor = true;
+            this.BTN_OK.Click += OnClick_OK;
+            // 
+            // BTN_COPY
+            // 
+            this.BTN_COPY.Location = new System.Drawing.Point(177, 76);
+            this.BTN_COPY.Name = "BTN_COPY";
+            this.BTN_COPY.Size = new System.Drawing.Size(75, 23);
+            this.BTN_COPY.TabIndex = 3;
+            this.BTN_COPY.Text = "복사";
+            this.BTN_COPY.UseVisualStyleBackColor = true;
+            this.BTN_COPY.Click += OnClick_Copy;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 111);
+            this.Controls.Add(this.BTN_COPY);
+            this.Controls.Add(this.BTN_OK);
+            this.Controls.Add(this.TBOX_PName);
+            this.Controls.Add(this.TBOX_PID);
+            this.Name = "Form1";
+            this.Text = "Process Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBOX_PID;
+        private System.Windows.Forms.TextBox TBOX_PName;
+        private System.Windows.Forms.Button BTN_OK;
+        private System.Windows.Forms.Button BTN_COPY;
+    }
+}
+
---
+++ proj/WinForms/ProcessChecker/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProcessChecker
+{
+    using System.Diagnostics;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnClick_OK(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(TBOX_PID.Text))
+            {
+                MessageBox.Show("PID를 입력하세요!", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var PID = int.Parse(TBOX_PID.Text);
+
+            var proc = Process.GetProcessById(PID);
+            if (null == proc)
+            {
+                MessageBox.Show($"해당 게임이 없습니다. PID({PID})를 확인하세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            TBOX_PName.Text = proc.ProcessName;
+            MessageBox.Show("앱을 찾았습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void OnClick_Copy(object sender, EventArgs e)
+        {
+            //Console.WriteLine("OnClick_Copy(): ");
+            Clipboard.SetText(TBOX_PName.Text);
+            MessageBox.Show("클립보드에 복사하였습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
