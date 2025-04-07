3db205b75bb3a69e98fa1722d32279a6d2d2635a
coder-lloyd <coder.lloyd01@gmail.com>
2025-04-07 18:26:21 +0900

    [proj-DEV APP] ## : APP UI * 프레임 파츠 별 분리

--- proj/_devapp_/main.py
+++ proj/_devapp_/main.py
@@ -8,7 +8,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.append(current_dir)
 
-from ui.app_ui import AutomationAppUI
+from zzz.app_ui import AutomationAppUI
 from core.settings_manager import SettingsManager
 from zzz.config import *
 
