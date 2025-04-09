aeaef37368b98a4c40bb5b14fad6412dac780937
coder-lloyd <coder.lloyd01@gmail.com>
2025-04-09 22:26:02 +0900

    [proj-DEV APP] ## : stores-default info * 기본 정보  *

--- proj/_devapp_/core/capture_utils.py
+++ proj/_devapp_/core/capture_utils.py
@@ -9,6 +9,7 @@ from core.ocr_engine import image_to_text
 
 from zzz.config import LOOP_TEXT_KEYWORD
 from stores.areas import *
+from stores.def_info import Update_Value
 
 class CaptureManager:
     """화면 캡처 관리 클래스 (mss 라이브러리 기반)"""
@@ -54,16 +55,17 @@ class CaptureManager:
         # 각 스레드에서 새로운 MSS 인스턴스를 생성
         with mss.mss() as sct:
             while self.is_capturing:
-                try:
-                    # 창이 여전히 존재하는지 확인
-                    if not self.window_manager.update_window_info():
-                        if self.callback_fn:
-                            self.callback_fn("error", "창이 닫혔습니다.")
-                        self.is_capturing = False
-                        break
+                # 창이 여전히 존재하는지 확인
+                if not self.window_manager.update_window_info():
+                    if self.callback_fn:
+                        self.callback_fn("error", "창이 닫혔습니다.")
+                    self.is_capturing = False
+                    break
 
-                    for n in range(len(LOOP_TEXT_KEYWORD)):
-                        KEY = LOOP_TEXT_KEYWORD[n];
+                for n in range(len(LOOP_TEXT_KEYWORD)):
+                    KEY = LOOP_TEXT_KEYWORD[n];
+                    
+                    try:
                         area = Get_TextArea(KEY)
                         if area is None:
                             continue
@@ -83,20 +85,20 @@ class CaptureManager:
                             "height": height
                         }
                         screenshot = sct.grab(monitor)
-                    # mss의 결과를 PIL Image로 변환
+                        # mss의 결과를 PIL Image로 변환
                         img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
 
-                    # OCR 실행
+                        # OCR 실행
                         if img is None:
                             raise ValueError("캡처된 이미지가 None입니다.")
                         text = image_to_text(img)
                         del img
                         import gc; gc.collect()
                     
-                    # 디버깅 정보 출력
-                    # print(f"인식된 텍스트: {text}")
-                    
-                    # 콜백 함수 호출
+                        # 디버깅 정보 출력
+                        # print(f"인식된 텍스트: {text}")
+                        
+                        # 콜백 함수 호출
                         if self.callback_fn:
                             timestamp = time.strftime("%H:%M:%S", time.localtime())
                         # self.callback_fn("result", f"[{timestamp}] 인식 결과:\n{text}\n{'='*50}\n")
@@ -104,11 +106,14 @@ class CaptureManager:
                             if not text:
                                 logtext += "\n"
                             self.callback_fn("result", logtext)
+                            
+                            Update_Value(KEY, text)
 
-                except Exception as e:
-                    # print(f"[캡처 오류] {type(e).__name__}: {str(e)}")
-                    if self.callback_fn:
-                        self.callback_fn("error", f"오류 발생: {str(e)}")
+                    except Exception as e:
+                        Update_Value(KEY, "")
+                        # print(f"[캡처 오류] {type(e).__name__}: {str(e)}")
+                        # if self.callback_fn:
+                        #     self.callback_fn("error", f"오류 발생: {str(e)}")
                 
                 # 지정된 간격만큼 대기
                 time.sleep(self.capture_interval)
---
+++ proj/_devapp_/stores/def_info.py
@@ -0,0 +1,60 @@
+HP = 0
+MP = 0
+
+POTION = 0
+Is_Potion0 = False
+
+Locate_Kind = ""
+Locate_Name = ""
+
+def Update_Value(key, text):
+    text = text.replace("\n", "").replace("\r", "")  # 줄바꿈 제거
+    
+    match key:
+        case "스탯:피통": _Set_HP(text)
+        case "스탯:마나통": _Set_MP(text)
+        case "스탯:물약": _Set_Potion(text)
+        # case "스탯:물약-엥꼬": Is_Potion0 = False
+        
+        case "지역:종류":
+            global Locate_Kind
+            Locate_Kind = text
+            print(f"Locate_Kind= {Locate_Kind}")
+        case "지역:이름":
+            global Locate_Name
+            Locate_Name = text
+            print(f"Locate_Name= {Locate_Name}")
+
+def _Set_HP(text):
+    global HP
+    HP = _Parse_Vital("HP", text)
+    print(f"HP= {HP}")
+    
+def _Set_MP(text):
+    global MP
+    MP = _Parse_Vital("MP", text)
+    print(f"MP= {MP}")
+    
+def _Set_Potion(text):
+    global POTION
+    try:
+        strvar = text.replace(",", "").replace(".", "")
+        POTION = int(strvar)
+    except Exception as e:
+        POTION = -1;
+    print(f"POTION= {POTION}")
+
+def _Parse_Vital(kind, text):
+    if text:
+        try:
+            # 문자열 분리 및 정리
+            curr = text.split("/")[0].strip()  # 왼쪽 값 (현재 HP)
+            curr = curr.replace(",", "").replace(".", "")
+
+            # 숫자로 변환
+            return int(curr)
+
+        except Exception as e:
+            # print(f"[kind] 입력값 '{strValue}' 처리 실패: {e}")
+            return -1
+    else: return -1
