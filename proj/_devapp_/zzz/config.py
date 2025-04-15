1bd418befb140271b6e07d114ae61269b07cda26
coder-lloyd <coder.lloyd01@gmail.com>
2025-04-15 11:17:06 +0900

    [proj-DEV APP] ##: DEV 타겟 게임 * config: DEFAULT_APP_NAME  * 'RF 온라인 넥스트'로 변경

--- proj/_devapp_/zzz/config.py
+++ proj/_devapp_/zzz/config.py
@@ -17,7 +17,7 @@ APP_THEME = 'classic'
 # aqua: macOS의 기본
 
 # 기본 입력값
-DEFAULT_APP_NAME = "LORDNINE"  # 기본 검색할 앱 이름
+DEFAULT_APP_NAME = "RF 온라인 넥스트"  # 기본 검색할 앱 이름
 DEFAULT_PID = ""             # 기본 PID (비워두기)
 DEFAULT_CAPTURE_X = "0"      # 기본 캡처 X 좌표
 DEFAULT_CAPTURE_Y = "0"      # 기본 캡처 Y 좌표
