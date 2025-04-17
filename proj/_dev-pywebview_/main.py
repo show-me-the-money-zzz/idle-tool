7274a13d39b18c442545dbf536352dacf8df374b
coder-lloyd <coder.lloyd01@gmail.com>
2025-04-18 05:00:47 +0900

    [proj-PyWebView] ##: 타이틀 *  *

--- proj/_dev-pywebview_/main.py
+++ proj/_dev-pywebview_/main.py
@@ -3,5 +3,5 @@ from backend_api import Api
 
 if __name__ == '__main__':
     api = Api()
-    window = webview.create_window('PyWebView App', 'web/index.html', js_api=api)
+    window = webview.create_window('쌀먹툴', 'web/index.html', js_api=api)
     webview.start(debug=True)
