686c20d83601c4b2c7ba577d0b9877194cfcddf8
coder-lloyd <coder.lloyd01@gmail.com>
2025-04-27 22:34:46 +0900

    [proj-DEV APP] ##: grinder_types.SelectedTask * 분리

---
+++ proj/_devapp_/grinder_types/selected_task.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import copy
+
+import stores.task_manager as TaskMan
+
+@dataclass
+class SelectedTask:
+    origin_key: str #task의 이름 변경 대비 (사냥1이 origin_key / 사냥XX current_key)
+    current_key: str    
+    task: Optional[TaskMan.Task]    # deepcopy(task) 컨트롤 변경을 일일이 업데이트
+    # task: TaskMan.Task | None
+    origin_step_key: str    #step의 이름 변경 대비 (잡화상점찾기 origin_step_key / 잡화상점찾기가기 current_step_key)
+    current_step_key: str
+    
+    # # 변경 전에 저장된 데이터 있는지 확인
+    # ## selected_originkey 가 원본에 없는 key이면 추가
+    # ## 있는 key면 selected_currentkey / selected_currenttask으로 비교해서 변경사항 추적
+    def Set_Task(self, key, task: TaskMan.Task | None):
+        self.origin_key = key
+        self.current_key = key
+        if task:
+            self.task = copy.deepcopy(task)
+        else: self.task = None
+    def ChangeKey_CurrentTask(self, key):
+        self.current_key = key
+
+    def Set_StepKey(self, key):
+        self.origin_step_key = key
+        self.current_step_key = key
+    def ChangeKey_CurrentStep(self, key):
+        self.current_step_key = key
+    def Get_Step(self): #origin_step_key로 가져오기
+        return self.task.Get_Step(self.origin_step_key)
