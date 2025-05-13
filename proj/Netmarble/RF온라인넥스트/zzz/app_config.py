7f0306880b615769111ca452da3f5c4b993497f1
coder-lloyd <coder.lloyd01@gmail.com>
2025-05-13 14:06:44 +0900

    [Netmarble\RF온라인넥스트] ##: 업데이트 * rev. 673

--- proj/Netmarble/RF온라인넥스트/zzz/app_config.py
+++ proj/Netmarble/RF온라인넥스트/zzz/app_config.py
@@ -2,7 +2,7 @@ RELEASE_APP = True
 # 개발용 앱 구분
 
 # 앱 기본 설정
-APP_VERSION="0.2.651"
+APP_VERSION="0.2.673"
 
 # APP_TITLE = "쌀먹 - [게임]" # 연결하려는 앱의 타이틀이 포함되면 안됨
 APP_TITLE = f"쌀먹툴 - RF온라인넥스트" # 연결하려는 앱의 타이틀이 포함되면 안됨
